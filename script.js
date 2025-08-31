e2d874742cebb0edf7a9987d47af5721b1fd6dad
Syed <finexgamer4@gmail.com>
2025-08-31 22:47:11 +0600

    Completed Challenge

---
+++ script.js
@@ -0,0 +1,23 @@
+const sec = document.getElementById("section-con")
+fetch('./data.json')
+    .then(res => {
+        if (!res.ok) {
+            console.log("Problem");
+            return;
+        }
+        return res.json();
+    })
+    .then(data => {
+        data.forEach(post => {
+            const markup = `
+            <div class="section ${post.category}">
+            <div class="icon"><img src="${post.icon}" alt=""></div>
+            <div class="value-container">
+              <p class="attribute">${post.category}</p> <span><b>${post.score}</b><span class="total-value">/ 100</span></span>
+            </div>
+          </div>
+        ` ;
+            sec.insertAdjacentHTML('beforeend', markup)
+        });
+    })
+    .catch(error => console.log(error));
